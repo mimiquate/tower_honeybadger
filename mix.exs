36d4f6a919bf033b1724ed33b27b9f2fa9d10d69
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-12 14:29:30 -0300

    ci: enable

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TowerHoneybadger.MixProject do
     [
       app: :tower_honeybadger,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
