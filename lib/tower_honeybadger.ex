219f2969451905899059d390bd669b27c78a9fe2
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-12 14:13:38 -0300

    mix new tower_honeybadger

---
+++ lib/tower_honeybadger.ex
@@ -0,0 +1,18 @@
+defmodule TowerHoneybadger do
+  @moduledoc """
+  Documentation for `TowerHoneybadger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TowerHoneybadger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tower_honeybadger_test.exs
@@ -0,0 +1,8 @@
+defmodule TowerHoneybadgerTest do
+  use ExUnit.Case
+  doctest TowerHoneybadger
+
+  test "greets the world" do
+    assert TowerHoneybadger.hello() == :world
+  end
+end
