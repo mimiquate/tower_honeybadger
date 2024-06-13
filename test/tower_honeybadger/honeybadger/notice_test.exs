bccb164fcf289b614e4ae5609391363e6efcf4a1
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-13 10:24:44 -0300

    test: Honeybadger.Notice.from_exception

---
+++ test/tower_honeybadger/honeybadger/notice_test.exs
@@ -0,0 +1,71 @@
+defmodule TowerHoneybadger.Honeybadger.NoticeTest do
+  use ExUnit.Case
+  doctest TowerHoneybadger.Honeybadger.Notice
+
+  alias TowerHoneybadger.Honeybadger
+
+  test "from_exception" do
+    Application.put_env(:tower_honeybadger, :environment, "test")
+
+    notice =
+      try do
+        raise "a test"
+      rescue
+        exception in RuntimeError ->
+          Honeybadger.Notice.from_exception(exception, __STACKTRACE__)
+      end
+
+    assert %{
+             "error" => %{
+               "class" => "RuntimeError",
+               "message" => "a test",
+               "backtrace" => [
+                 %{
+                   "method" =>
+                     ~s(TowerHoneybadger.Honeybadger.NoticeTest."test from_exception"/1),
+                   "file" => "test/tower_honeybadger/honeybadger/notice_test.exs",
+                   "number" => 12
+                 },
+                 %{
+                   "method" => _,
+                   "file" => "lib/ex_unit/runner.ex",
+                   "number" => _
+                 },
+                 %{
+                   "method" => _,
+                   "file" => "timer.erl",
+                   "number" => _
+                 },
+                 %{
+                   "method" => _,
+                   "file" => "lib/ex_unit/runner.ex",
+                   "number" => _
+                 }
+               ]
+             },
+             "server" => %{
+               "environment_name" => "test"
+             }
+           } = notice
+  end
+
+  #
+  # test "from_message" do
+  #   Application.put_env(:tower_honeybadger, :environment, "test")
+  #
+  #   notice = Honeybadger.Notice.from_message("something interesting happened")
+  #
+  #   assert %{
+  #            "data" => %{
+  #              "environment" => "test",
+  #              "timestamp" => _,
+  #              "level" => "info",
+  #              "body" => %{
+  #                "message" => %{
+  #                  "body" => "something interesting happened"
+  #                }
+  #              }
+  #            }
+  #          } = notice
+  # end
+end
