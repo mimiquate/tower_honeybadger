0868b3aefe249c912b6f21e584d6286085a8d996
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-09-23 14:31:58 -0300

    feat: inform about unsupported message kind

--- lib/tower_honeybadger/reporter.ex
+++ lib/tower_honeybadger/reporter.ex
@@ -2,6 +2,13 @@ defmodule TowerHoneybadger.Reporter do
   @behaviour Tower.Reporter
 
   @impl true
+  def report_event(%Tower.Event{kind: :message}) do
+    if enabled?() do
+      IO.puts("TowerHoneybadger DOES NOT support reporting messages, ignoring...")
+    else
+      IO.puts("TowerHoneybadger NOT enabled, ignoring...")
+    end
+  end
 
   def report_event(%Tower.Event{} = tower_event) do
     if enabled?() do
