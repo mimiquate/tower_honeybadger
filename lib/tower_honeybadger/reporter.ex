ef4200b22d8568b419a48ff33c95ad3fe8fce74e
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-08-23 12:34:47 -0300

    keep it async

--- lib/tower_honeybadger/reporter.ex
+++ lib/tower_honeybadger/reporter.ex
@@ -1,6 +1,8 @@
 defmodule TowerHoneybadger.Reporter do
   @moduledoc false
 
+  alias TowerHoneybadger.Honeybadger
+
   def report_event(%Tower.Event{kind: :message}) do
     if enabled?() do
       IO.puts("Honeybadger DOES NOT support reporting messages, ignoring...")
@@ -11,9 +13,11 @@ defmodule TowerHoneybadger.Reporter do
 
   def report_event(%Tower.Event{} = tower_event) do
     if enabled?() do
-      tower_event
-      |> TowerHoneybadger.Honeybadger.Notice.from_tower_event()
-      |> TowerHoneybadger.Honeybadger.Client.send()
+      notice = Honeybadger.Notice.from_tower_event(tower_event)
+
+      async(fn ->
+        Honeybadger.Client.send(notice)
+      end)
     else
       IO.puts("TowerHoneybadger NOT enabled, ignoring...")
     end
@@ -21,5 +25,10 @@ defmodule TowerHoneybadger.Reporter do
 
   defp enabled? do
     !!Application.fetch_env!(:tower_honeybadger, :api_key)
+  end
+
+  defp async(fun) do
+    Tower.TaskSupervisor
+    |> Task.Supervisor.start_child(fun)
   end
 end
